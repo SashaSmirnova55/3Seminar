f730f480fb2069bd02aca5df1a5d0b17d4a0eff9
SashaSmormova <zorrogirl@mail.ru>
2023-06-24 13:03:09 +0300

    + 2 задачи

---
+++ Task_021/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3, 6, 8); B (2, 1, -7) -> 15.84
+// A (7, -5, 0); B (1, -1, 9) -> 11.53
+
+System.Console.WriteLine("Введите через пробел координаты первой точки: ");
+int[] A = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+System.Console.WriteLine("Введите через пробел координаты второй точки: ");
+int[] B = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+System.Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow(B[0]-A[0], 2)+Math.Pow(B[1]-A[1], 2)+Math.Pow(B[2]-A[2], 2)), 2));
---
+++ Task_023/Program.cs
@@ -0,0 +1,13 @@
+﻿﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= number)
+{
+    System.Console.Write(Math.Pow(count, 3) + " ");
+    count++;
+} 
